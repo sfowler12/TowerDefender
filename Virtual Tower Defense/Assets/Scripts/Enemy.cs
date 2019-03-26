70d2395078d6bc8011ad7179db225b35d8f17720
ocolina6720 <oscarhill14@hotmail.com>
2019-03-25 21:28:53 -0500

    Added Enemy path logic
    - Moved camera position / angle
    - Made Materials, Prefabs, Scripts  folders
    - Added groundplane & waypoints Objects to Scene
    - Made enemy prefabs
    - Made waypoints & Enemy Scripts

---
+++ Virtual Tower Defense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+	public float speed;
+
+	private Transform target;
+	private int waypointIndex =0;
+
+	void Start(){
+		target = waypoints.points[0];
+	}
+
+	void Update(){
+		Vector3 dir = target.position - transform.position;
+		transform.Translate(dir.normalized * speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f) {
+            GetNextWaypoint();
+        }
+	}
+
+    void GetNextWaypoint() {
+        if (waypointIndex >= waypoints.points.Length - 1) {
+            Destroy(gameObject);
+            return;
+        }
+        waypointIndex++;
+        target = waypoints.points[waypointIndex];
+    }
+}
